35ac6b1a0c046212e4e849f782bd05a50d2b7f1f
Chris Abiva <chrisabiva@hotmail.com>
2021-08-26 18:09:27 -0700

    Added footer and create new event form (still not working.)

--- client/src/pages/NewEvents/index.js
+++ client/src/pages/NewEvents/index.js
@@ -1,54 +1,52 @@
-// // import React, { useState } from 'react';
-// // import TextField from '@material-ui/core/TextField';
-// // import { makeStyles } from '@material-ui/core/styles';
-// // import './style.css';
+import React, { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import './style.css';
 
-// // const UserProfile = () => {
-// //   const [formState, setFormState] = useState({ email: '', password: '' });
+const UserProfile = () => {
+  const [formState, setFormState] = useState({ email: '', password: '' });
 
-//   const handleFormSubmit = async (event) => {
-//     event.preventDefault();
-//     const mutationResponse = await addUser({
-//       variables: {
-//         email: formState.email,
-//         password: formState.password,
-//         firstName: formState.firstName,
-//         lastName: formState.lastName,
-//       },
-//     });
+  // const handleFormSubmit = async (event) => {
+  //   event.preventDefault();
+  //   const mutationResponse = await addUser({
+  //     variables: {
+  //       email: formState.email,
+  //       password: formState.password,
+  //       firstName: formState.firstName,
+  //       lastName: formState.lastName,
+  //     },
+  //   });
 
-
+// Organizer 
+// Created @
   
-// //   return (
-// //     <div className="container">
-// //       <form>
-// //         <div>
-// //           <label htmlFor="firstName">First Name:</label>
-// //           <TextField required id="first-name" label="Required" defaultValue="First Name" />
-// //         </div>
-// //         <div>
-// //           <label htmlFor="lastName">Last Name:</label>
-// //           <TextField required id="last-name" label="Required" defaultValue="Last Name" />
-// //         </div>
-// //         <div>
-// //           <label htmlFor="email">Email:</label>
-// //           <TextField required id="email-input" label="Required" defaultValue="Email" />
-// //         </div>
-        
-// //         <div>
-// //           <label htmlFor="password">Password:</label>
-// //           <TextField
-// //           id="standard-password-input"
-// //           label="Password"
-// //           type="password"
-// //           autoComplete="current-password"
-// //           />  
-// //         </div>
-        
-        
-// //       </form>
-// //     </div>
-// //   );
-// // };
+  return (
+    <div className="container">
+      <form>
 
-// // export default UserProfile;
+      <h1>Create New Event</h1>
+        <div>
+          <label htmlFor="title">Title:</label>
+          <TextField required id="title" label="Required" defaultValue="title" />
+        </div>
+        <div>
+          <label htmlFor="location">Location:</label>
+          <TextField required id="location" label="Required" defaultValue="Joey's Restaurant" />
+        </div>
+        <div>
+          <label htmlFor="email">Address:</label>
+          <TextField required id="email-input" label="Required" defaultValue="800 Bellevue Way NE Ste 118" />
+        </div>
+        <div>
+          <label htmlFor="zipcode">Zipcode:</label>
+          <TextField required id="zipcode-input" label="Required" defaultValue="98055" />
+        </div>
+        
+        
+        
+      </form>
+    </div>
+  );
+};
+
+export default UserProfile;
