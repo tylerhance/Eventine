f4962e9f0f594894031e2d97bfa6c600e28a4e7b
Chris Abiva <chrisabiva@hotmail.com>
2021-08-26 20:34:34 -0700

    pulling from develop

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -18,6 +18,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Image from '../../assets/images/concert.jpeg';
+import Login from './oldlogin';
 
 
 // function Login(props) {
@@ -196,3 +197,4 @@ export default function SignInSide() {
   );
 }
 
+export default Login;
